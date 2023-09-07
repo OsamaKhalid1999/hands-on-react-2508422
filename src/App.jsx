5d2f7de4da9f45189d2772187526ca60e44c7a09
Osama_Khalid_99 <72207013+OsamaKhalid1999@users.noreply.github.com>
2023-09-07 12:52:27 +0000

    personal adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import './App.css';
+
+const App = () => {
+  const name = "StarGazers"
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className = "container">  
+    <article>
+      <hgroup>
+        <img src = "images/group.png" alt = "StarGazers Group" />
+        <h1 className='firstH1'>Meet the <i className = {'hiii'} id={'bye'}>{name}</i></h1>
+        <h1 className='firstH1 secondH1'>Meet the <i className = {'hiii'} id={'bye'}>{name}</i></h1>
+        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        <button classname = "outline" onClick = {()=> alert('Hi There')}></button>
+      </hgroup>
+    </article>     
+    </div>
+    
   )
 }
 export default App
